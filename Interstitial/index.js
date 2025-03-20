eb5d55a471ec5325ba397ed4718ec5b8080d0ffb
Michael2001papis <dvnka2@gmail.com>
2025-03-20 23:55:21 +0200

    56347856983745

--- Interstitial/index.js
+++ Interstitial/index.js
@@ -40,7 +40,7 @@
       
     
       document.getElementById("rights").addEventListener("click", () => {
-        window.location.href = "Developerrights/index.html";
+        window.location.href = "../Developerrights/index.html";
       });
   });
   
